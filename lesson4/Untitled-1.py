4bff5028c356eaae57018bd4fd6eb7a539f8e306
unknown <agentkiril911@gmail.com>
2022-04-10 13:51:32 +0300

    DZ2

---
+++ lesson4/Untitled-1.py
@@ -0,0 +1,10 @@
+print('Hello world')
+name = input("Ваше імя?")
+greeting = "Привіт" + name
+print(greeting)
+surnames = input("ваше прізвище")
+greeting = "Добрий день" + name + surnames
+print(greeting)
+place = input("Де ви зараз знаходитесь?")
+greeting = "добрий день з..." + place
+print(greeting)
